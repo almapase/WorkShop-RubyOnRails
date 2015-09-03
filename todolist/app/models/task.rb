95b44dcafec48d0752e75ca086c83da83cbaf1b1
Alex Pacheco Segura <alex.pacheco.s@gmail.com>
2015-09-02 22:02:33 -0300

    Scope y after_save

--- todolist/app/models/task.rb
+++ todolist/app/models/task.rb
@@ -1,3 +1,22 @@
 class Task < ActiveRecord::Base
   belongs_to :group
+  #where
+  scope :all_done, -> {where(done: true)}
+
+  after_save :send_mail
+
+  #consultar datos de la tabla relacionada
+  delegate :name, to: :group, prefix: true, allow_nil: true
+
+  def send_mail
+    puts "envio mail"
+  end
+
+  def set_done
+    self.done = true
+  end
+
+  def set_done!
+    self.done = true
+  end
 end
